1881be75a1b40ef39a7c7ea9677b4f7b2379c1ca
AlisaMa1 <102972968+AlisaMa1@users.noreply.github.com>
2025-03-03 17:17:24 +0100

    Add files via upload

---
+++ MealPath.OrderManagement.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using MealPath.OrderManagement.Domain.Common;
+
+namespace MealPath.OrderManagement.Domain.Entities
+{
+    public class Product : AuditableEntity
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
