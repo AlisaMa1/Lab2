22bc9d21174b985ea7ee957be6250d3dd00bc7ff
Fluturaibrahimi6 <96262264+Fluturaibrahimi6@users.noreply.github.com>
2025-03-05 12:58:20 +0100

    Add files via upload

---
+++ MealPath.OrderManagement.Application/Contracts/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace MealPath.OrderManagement.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> predicate);
+        Task<(int totalCount, IReadOnlyList<T> data)> GetPagedReponseAsync(int page, int size);
+
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
