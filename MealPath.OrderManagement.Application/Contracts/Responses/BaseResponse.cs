286f6de11d1f6799de2c1c942551af50bf418ead
Fluturaibrahimi6 <96262264+Fluturaibrahimi6@users.noreply.github.com>
2025-03-05 12:59:40 +0100

    Add files via upload

---
+++ MealPath.OrderManagement.Application/Contracts/Responses/BaseResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace MealPath.OrderManagement.Application.Responses
+{
+    public class BaseResponse<T> where T : class
+    {
+        public BaseResponse()
+        {
+            Success = true;
+        }
+        public BaseResponse(string message)
+        {
+            Success = true;
+            Message = message;
+        }
+
+        public BaseResponse(string message, bool success)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public List<string>? ValidationErrors { get; set; }
+        
+        public T? Value { get; set; }
+    }
+}
